ba4aac4ecc1fbbfb2d1ed2626d977c9a5b2c803d
Portagens <ricardo.j.jan@gmail.com>
2023-01-09 11:07:11 +0300

    Purchase card fucntion update

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -6,10 +6,10 @@ pragma solidity ^0.8.17;
 
 contract TurkishFootballCards 
 {
-    string owner;
+    address payable TF_owner;
     address nftMinter;
-
-    
+    uint nftCount = 0;
+    mapping(uint => card) public nfts;
     
     struct card
     {
@@ -17,12 +17,13 @@ contract TurkishFootballCards
         address owner;
         address nftID; //not sure if address, will check later
         bool soldBefore;
+        uint price;
     }
 
 
     constructor()
     {
-        owner = "All rights belong to the turkish football federation";
+        TF_owner = payable(0x08B9F93cf5bde9dDEf4E4BF54df2aD5A9902f744);
         //nftMinter = 'address of the turkish football federation'; 
 
     }
@@ -36,14 +37,24 @@ contract TurkishFootballCards
     function purchaseCard(uint _id) public payable
     {
         //check correct id
+        require(_id >= 0 && _id <= nftCount);
         //check if valid creator/current owner
+        card memory _nft_card = nfts[_id]; 
+        require(_nft_card.owner == TF_owner);
         //check if it's been sold
+        require(!_nft_card.soldBefore);
         //check price/funds
-        
+        require(msg.value >= _nft_card.price);
         //change owner
+        _nft_card.owner = msg.sender;
         //mark as sold
+        _nft_card.soldBefore = true;
+        //update nft list
+        nfts[_id] = _nft_card;
         //transfer funds
+        TF_owner.transfer(msg.value);
         //trigger event
+        
     }
 
     
