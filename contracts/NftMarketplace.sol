ea22460afd136ef410a2da7da4a753bf58a62ef4
Portagens <ricardo.j.jan@gmail.com>
2023-01-09 19:42:29 +0300

    Added minting function and changed selling function to work with the ERC721

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -1,36 +1,38 @@
-pragma solidity ^0.8.17;
+pragma solidity >=0.8.17;
 
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 /// @notice Smart Contract for the nft marketplace of the turkish football assocition
 /// @dev Must receive payments for nft's (sell nfts), verify their creators id, create nfts
 
-contract TurkishFootballCards 
+contract TurkishFootballCards is ReentrancyGard
 {
-    address payable TF_owner;
-    address nftMinter;
-    uint nftCount = 0;
-    mapping(uint => card) public nfts;
+    address public payable TF_owner;
+    uint256 public mintPrice = 0.002 ether
+    uint256 nftCount = 0;
+    mapping(uint256 => card) public nfts;
+
     
     struct card
     {
-        uint id;
+        uint256 id;
         address owner;
-        address nftID; //not sure if address, will check later
-        bool soldBefore;
-        uint price;
+        bool soldBefore; 
+        uint256 price;
     }
 
     event nft_sold
-    {
-        uint id;
-        address owner;
-        address nftID; 
-        bool soldBefore;
-        uint price;
-    }
+    (
+        uint256 id,
+        address owner,
+        address nftID, 
+        bool soldBefore,
+        uint265 price
+    );
 
 
-    constructor()
+    constructor TFF_Marketplace() is ReentrancyGuard
     {
         TF_owner = payable(0x08B9F93cf5bde9dDEf4E4BF54df2aD5A9902f744);
         //nftMinter = 'address of the turkish football federation'; 
@@ -38,13 +40,31 @@ contract TurkishFootballCards
     }
 
     
-    function createCard() public
+    function mint(uint256 _nft_price) external onlyOwner payable
     {
+        //this funtion is responsible for the minting of new coins
+        //only the Turkish Football federation is supposed to be able to mint
+        require(msg.sender.address == TF_owner)
+        //the message value should be equal to the minting cost
+        require(msg.value == mintPrice);
+        
+        //creating the new nft in the marketplace
+        card _new_nft(nftCount, TF_owner, false, _nft_price);
+        nfts[nftCount] = _new_nft;
+        nftCount++;
+
+        //actually creating the nft in the
+        uint256 TokenID = nftCount;
+        _safeMint(TF_owner, TokenID); //emits a {Transfer} event
+
 
 
     }
-    
-    function purchaseCard(uint _id) public payable
+
+    //this version of the funtion in gonna diiiieee
+    //this is a normal marketplace, now, let us change it to a nft marketplace
+
+    function purchaseCard(uint256 _id) public external payable
     {
         //check correct id
         require(_id >= 0 && _id <= nftCount);
@@ -56,7 +76,7 @@ contract TurkishFootballCards
         //check price/funds
         require(msg.value >= _nft_card.price);
         //change owner
-        _nft_card.owner = msg.sender;
+        //_nft_card.owner = msg.sender; we should use the proper thing to t«do this, aka ERC721
         //mark as sold
         _nft_card.soldBefore = true;
         //update nft list
@@ -64,7 +84,30 @@ contract TurkishFootballCards
         //transfer funds
         TF_owner.transfer(msg.value);
         //trigger event
-        emit(_id, _nft_card.owner,_nft_card.nftID, true, _nft_card.price);
+        emit nft_sold(_id, _nft_card.owner,_nft_card.nftID, true, _nft_card.price);
+
+    }
+    //new version of the purchaseCard, using the standard ERC721
+    function purchaseCard(uint256 _tokenID) public external payable
+    {
+        //check correct id
+        require(_tokenID >= 0 && _id <= nftCount);
+        //load nft information
+        address owner = ownerOf(_tokenID);
+        cards cardToSell = nfts[_tokenID];
+        require(owner == TF_owner, "Invalid owner, nft does not belong to the Turkish football association");
+        //check for funds 
+        require(msg.value >= cardToSell.price, "Unsufficient funds");
+        //tranfer the funds (fund value can change after calling external functions, aka the safeTransferFrom)
+        TF_owner.transfer(msg.value); // maybe not use this because of the Istambull fork and change in gas prices (not safe anymore)
+        //transfer the nft
+        safeTransferFrom(owner, msg.sender(),_tokenID);
+        //update the cards info
+        cardToSell.owner = msg.sender;
+        cardToSell.soldBefore = true;
+        nfts[_tokenID] = cardToSell;
+        //event to log this, maybe not do it, cause the safeTransferFrom function already emits a {Transfer} event
+        emit nft_sold(_tokenID, _nft_card.owner,_nft_card.nftID, true, _nft_card.price);
 
     }
 
